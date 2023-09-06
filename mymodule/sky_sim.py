0c4088b9b7dd6e619972eba0d08c3f291618f5df
PaulHancock <paul.hancock@curtin.edu.au>
2023-09-06 14:41:32 +0800

    include logging

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -4,6 +4,11 @@
 from math import cos, pi
 from random import uniform
 import argparse
+import logging
+
+logging.basicConfig(format='%(asctime)s %(name)s %(levelname)-8s %(message)s', level=logging.DEBUG)
+log = logging.getLogger("mylogger")
+
 
 NSRC = 1_000_000
 # from wikipedia
@@ -23,6 +28,7 @@ def get_radec():
     dec : float
         The DEC, in degrees for Andromeda
     """
+    log.debug("Starting get_radec")
     # from wikipedia
     andromeda_ra = '00:42:44.3'
     andromeda_dec = '41:16:09'
@@ -33,6 +39,7 @@ def get_radec():
     h, m, s = andromeda_ra.split(':')
     ra = 15*(int(h)+int(m)/60+float(s)/3600)
     ra = ra/cos(dec*pi/180)
+    log.debug("Finished get_radec")
     return ra, dec
 
 
@@ -53,6 +60,7 @@ def make_positions(ra, dec, nsrc=NSRC):
     ras, decs : list
         A list of ra and dec coordinates.
     """
+    log.debug(f"Start make_positions with {ra}, {dec}, {nsrc}")
     # make 1000 stars within 1 degree of Andromeda
     ras = []
     decs = []
@@ -62,26 +70,30 @@ def make_positions(ra, dec, nsrc=NSRC):
     
     # apply our filter
     ras, decs = crop_to_circle(ras, decs, ra, dec, 1)
+    log.debug("Finished make_positions")
     return ras, decs
 
 
 def crop_to_circle(ras, decs, ref_ra, ref_dec, radius):
+    log.debug("Being cropping in crop_to_circle")
     ra_out = []
     dec_out = []
     for i in range(len(ras)):
         if (ras[i]-ref_ra)**2 + (decs[i]-ref_dec)**2 <= radius**2:
             ra_out.append(ras[i])
             dec_out.append(ras[i])
+    log.debug("finished cropping")
     return ra_out, dec_out
 
 
 def save_positions(ras, decs, out='catalog.csv'):
+    log.debug(f"Writing catalogue to {out}")
     # now write these to a csv file for use by my other program
     with open(out,'w', encoding='utf8') as f:
         print("id,ra,dec", file=f)
         for i in range(len(ras)):
             print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
-
+    log.info(f"Wrote file {out}")
 
 def skysim_parser():
     """
